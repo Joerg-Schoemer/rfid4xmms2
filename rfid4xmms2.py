dfd4218104f4bc617b94baf4151efdd16c4c53af
Jörg Schömer <joerg@joerg-schoemer.de>
2019-12-13 23:47:15 +0100

    create unknow file

--- rfid4xmms2.py
+++ rfid4xmms2.py
@@ -9,8 +9,9 @@ import pygame
 from pirc522 import RFID
 from pathlib import Path
 
-root="/home/pi/rfid4xmms2"
+root = "/home/pi/rfid4xmms2"
 commandsFolder = '{}/commands/'.format(root)
+unknownFolder = '{}/unknown/'.format(root)
 soundsFolder = '{}/sounds/'.format(root)
 
 run = True
@@ -19,11 +20,13 @@ rdr = RFID()
 lastCommandFileName = None
 allowedCommands = ['next', 'prev', 'pause', 'play', 'stop', 'toggle', 'album', 'title', 'url', 'volume']
 
+
 def logging(message):
     print(message)
     sys.stdout.flush()
 
-def playSound(file):
+
+def play_sound(file):
     pygame.init()
     pygame.mixer.init()
     pygame.mixer.music.load(file)
@@ -31,32 +34,39 @@ def playSound(file):
     while pygame.mixer.music.get_busy():
         time.sleep(0.2)
 
-def playErrorSound():
-    playSound(soundsFolder + 'no.mp3')
 
-def playSuccessSound():
-    playSound(soundsFolder + 'yes.mp3')
+def play_error_sound():
+    play_sound(soundsFolder + 'no.mp3')
+
+
+def play_success_sound():
+    play_sound(soundsFolder + 'yes.mp3')
+
 
 def xmms2cmd(cmd):
     subprocess.run(['su', 'pi', '-c xmms2 ' + cmd])
 
-def playAlbum(pattern):
+
+def play_album(pattern):
     xmms2cmd('stop')
     xmms2cmd('clear')
     xmms2cmd('add -o tracknr album:' + pattern)
     xmms2cmd('play')
 
-def playTitle(pattern):
+
+def play_title(pattern):
     xmms2cmd('stop')
     xmms2cmd('clear')
     xmms2cmd('add title:' + pattern)
     xmms2cmd('play')
 
-def playUrl(url):
+
+def play_url(url):
     xmms2cmd('stop')
     xmms2cmd('clear')
     xmms2cmd('add ' + url)
     xmms2cmd('play')
+
 
 def volume(direction):
     if direction == 'up':
@@ -65,19 +75,27 @@ def volume(direction):
         xmms2cmd('server volume -10')
     else:
         logging('invalid direction value \'' + direction + '\'')
-        playErrorSound()
+        play_error_sound()
 
-def generateCommandFileName(uid):
-    return commandsFolder + '_'.join("{:02X}".format(i) for i in uid) + '.cmd'
 
-def setLastCommandFileName(file):
+def generate_command_file_name(uid):
+    return commandsFolder + generate_file_name(uid)
+
+
+def generate_file_name(uid):
+    return '_'.join("{:02X}".format(i) for i in uid) + '.cmd'
+
+
+def set_last_command_file_name(file):
     global lastCommandFileName
     lastCommandFileName = file
 
-def commandFileNameNotChanged(commandFileName):
+
+def command_file_name_not_changed(commandFileName):
     return lastCommandFileName == commandFileName
 
-def end_read(signal,frame):
+
+def end_read(signal, frame):
     global run
     logging("\nCtrl+C captured, ending read.")
     run = False
@@ -85,14 +103,16 @@ def end_read(signal,frame):
     xmms2cmd('stop')
     sys.exit()
 
-def handle_hup(signal,frame):
+
+def handle_hup(signal, frame):
     logging("SIGHUP received")
+
 
 signal.signal(signal.SIGINT, end_read)
 
 signal.signal(signal.SIGHUP, handle_hup)
 
-playSuccessSound()
+play_success_sound()
 
 while run:
     lastReadTime = time.time()
@@ -101,51 +121,55 @@ while run:
 
     (error, data) = rdr.request()
     if error:
-        setLastCommandFileName(None)
+        set_last_command_file_name(None)
         continue
 
     (error, uid) = rdr.anticoll()
-    if not error:
-        commandFileName = generateCommandFileName(uid)
+    if error:
+        set_last_command_file_name(None)
+        continue
 
-        if time.time() - lastReadTime < 0.5 and commandFileNameNotChanged(commandFileName):
+    commandFileName = generate_command_file_name(uid)
+
+    if time.time() - lastReadTime < 0.5 and command_file_name_not_changed(commandFileName):
+        continue
+
+    cmdFilePath = Path(commandFileName)
+    if not cmdFilePath.is_file():
+        logging('command file \'' + commandFileName + '\' not found')
+        # creating unknown file
+        Path(unknownFolder + generate_file_name(uid)).touch()
+        play_error_sound()
+        set_last_command_file_name(commandFileName)
+        continue
+
+    with open(commandFileName) as file:
+        cmdAction = file.readline().strip()
+        if cmdAction not in allowedCommands:
+            logging('command \'' + cmdAction + '\' not allowed')
+            play_error_sound()
+            set_last_command_file_name(commandFileName)
             continue
 
-        cmdFilePath = Path(commandFileName)
-        if not cmdFilePath.is_file():
-            logging('command file \'' + commandFileName + '\' not found')
-            playErrorSound()
-            setLastCommandFileName(commandFileName)
-            continue
+        play_success_sound()
+        if cmdAction == 'album':
+            albumPattern = file.readline().strip()
+            play_album(albumPattern)
+            logging('playing album ' + albumPattern)
+        elif cmdAction == 'title':
+            titlePattern = file.readline().strip()
+            play_title(titlePattern)
+            logging('playing title ' + titlePattern)
+        elif cmdAction == 'url':
+            url = file.readline().strip()
+            play_url(url)
+            logging('playing url ' + url)
+        elif cmdAction == 'volume':
+            direction = file.readline().strip()
+            volume(direction)
+            logging('executing \'' + cmdAction + '\'' + ' direction=\'' + direction + '\'')
+        else:
+            xmms2cmd(cmdAction)
+            logging('executing \'' + cmdAction + '\'')
 
-        with open(commandFileName) as f:
-            cmd = f.readline().strip()
-            if not cmd in allowedCommands:
-                logging('command \'' + cmd + '\' not allowed')
-                playErrorSound()
-                setLastCommandFileName(commandFileName)
-                continue
-
-            playSuccessSound()
-            if cmd == 'album':
-                albumPattern = f.readline().strip()
-                playAlbum(albumPattern)
-                logging('playing album ' + albumPattern)
-            elif cmd == 'title':
-                titlePattern = f.readline().strip()
-                playTitle(titlePattern)
-                logging('playing title ' + titlePattern)
-            elif cmd == 'url':
-                url = f.readline().strip()
-                playUrl(url)
-                logging('playing url ' + url)
-            elif cmd == 'volume':
-                direction = f.readline().strip()
-                volume(direction)
-                logging('executing \'' + cmd +'\'' + ' direction=\'' + direction + '\'')
-            else:
-                xmms2cmd(cmd)
-                logging('executing \'' + cmd +'\'')
-        setLastCommandFileName(commandFileName)
-    else:
-        setLastCommandFileName(None)
+    set_last_command_file_name(commandFileName)

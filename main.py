d57c25d05217f968fc77817de46b6ea8abf11417
Schömer, Jörg <schoemer@adesso.de>
2021-01-07 22:13:46 +0100

    run on all inet devices

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ from rfid4xmms2.observer import doit
 if __name__ == '__main__':
     observer_process = Process(target=doit)
     observer_process.start()
-    application.run()
+    application.run(host='0.0.0.0', debug=False)

b07a31969c828b01eb282ee14ff0580e1fac9798
Schömer, Jörg <schoemer@adesso.de>
2021-03-10 07:09:20 +0100

    expression was inverted

--- scripts/convert_m4a_to_mp3.sh
+++ scripts/convert_m4a_to_mp3.sh
@@ -8,16 +8,16 @@ if [[ $(ls -1 *.m4a 2> /dev/null | wc -l) -eq 0 ]]; then
 fi
 
 if ! command -v parallel &> /dev/null; then
+  for f in *.m4a; do
+    ffmpeg -v 5 -y -i "$f" -codec:a libmp3lame -b:a 128k "$(basename "$f" .m4a).mp3"
+  done
+else
 (
 # build a list of commands to convert the m4a and execute it parallel
   for f in *.m4a; do
     echo ffmpeg -v 5 -y -i "\"$f\"" -codec:a libmp3lame -b:a 128k "\"$(basename "$f" .m4a).mp3\""
   done
 ) | parallel
-else
-  for f in *.m4a; do
-    ffmpeg -v 5 -y -i "$f" -codec:a libmp3lame -b:a 128k "$(basename "$f" .m4a).mp3"
-  done
 fi
 
 # remove all m4a files
